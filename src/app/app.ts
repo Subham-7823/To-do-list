ca8a536ca735d46a1d6ccd0d197263ebff698e51
Subhamjyoti Rana <subhamjyotiranagcsj@gmail.com>
2025-10-02 10:14:47 +0530

    Update app.ts
    updating ts file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,47 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('todolist');
+
+  task = "";
+  newtime = "";
+
+  taskList: { id: number, task: string, time: string }[] = [];
+
+  showRemainingOnly = false; // flag for button
+
+  addTask() {
+    if (this.task.trim() && this.newtime) {
+      this.taskList.push({
+        id: this.taskList.length + 1,
+        task: this.task.trim(),
+        time: this.newtime
+      });
+      this.task = '';
+      this.newtime = '';
+    }
+  }
+
+  deleteTask(taskId: number) {
+    this.taskList = this.taskList.filter(item => item.id !== taskId);
+  }
+
+  // ✅ Filter tasks after current time
+  get remainingTasks() {
+    const now = new Date();
+    const currentMinutes = now.getHours() * 60 + now.getMinutes();
+
+    return this.taskList.filter(task => {
+      const [h, m] = task.time.split(":").map(Number);
+      const taskMinutes = h * 60 + m;
+      return taskMinutes > currentMinutes;
+    });
+  }
 }
